368170279efe891bb1800a8ec1cd2cae1fa3faeb
hema2107 <66719586+hema2107@users.noreply.github.com>
2023-01-22 15:05:58 +0400

    Update train.py

--- train.py
+++ train.py
@@ -1,14 +1,22 @@
 !pip install kaggle
+
+
 /// upload json file
 from google.colab import files
 files.upload()
+
+
 
 !mkdir .kaggle
 !cp kaggle.json .kaggle/
 !chmod 600 .kaggle/kaggle.json 
 !mv .kaggle /root/
+
+
 // Dataset link
 !kaggle datasets download -d navoneel/brain-mri-images-for-brain-tumor-detection
+
+
 //Unzip the file
 from zipfile import ZipFile
 file_name = "/content/brain-mri-images-for-brain-tumor-detection.zip"
@@ -23,6 +31,9 @@ import tensorflow as tf
 data_dir = pathlib.Path('/content/brain_tumor_dataset')
 img_height=224
 img_width=224
+
+
+
 // Divide training and testing dataset
 train_ds  = tf.keras.utils.image_dataset_from_directory(
   data_dir,
@@ -32,6 +43,7 @@ train_ds  = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=32)
   
+  
   val_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -40,11 +52,13 @@ train_ds  = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=32)
   
+
 class_names = train_ds.class_names
 print(class_names)
 // plot the sample images
-import matplotlib.pyplot as plt
 
+
+import matplotlib.pyplot as plt
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
   for i in range(9):
@@ -52,6 +66,9 @@ for images, labels in train_ds.take(1):
     plt.imshow(images[i].numpy().astype("uint8"))
     plt.title(class_names[labels[i]])
     plt.axis("off")
+
+    
+    
 // Build the model    
 from tensorflow.keras.applications import DenseNet201
 from tensorflow.keras.models import Model
@@ -70,18 +87,23 @@ x = Dense(512, activation='relu')(x)
 x = Dropout(0.3)(x)
 predictions = Dense(2, activation='softmax')(x)
 model = Model(inputs=base_model.inputs, outputs=predictions)
+
+
+
 //Draw the model
 from keras.utils import plot_model
 plot_model(model, show_shapes=True)
+
 
 model.compile(
   optimizer='adam',
   loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
   metrics=['accuracy'])
   
+
+  
 from tensorflow.keras.callbacks import ModelCheckpoint
 # Checkpoint to save best model per epoch
-
 model_filepath = "/content/drive/My Drive/model-{epoch:02d}-{val_accuracy:.4f}.hdf5"
 checkpoint = ModelCheckpoint(
     filepath=model_filepath,
@@ -91,19 +113,20 @@ checkpoint = ModelCheckpoint(
     verbose=1
 )
 
+
+
 history=model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=25,  callbacks=[checkpoint])
   
+
 import matplotlib.pyplot as plt
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
-
 epochs = range(len(acc))
-
 plt.plot(epochs, acc, 'r', label='Training accuracy')
 plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
 plt.title('Training and validation accuracy')
@@ -115,6 +138,8 @@ plt.title('Training and validation loss')
 plt.legend()
 plt.show()
 
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -123,12 +148,16 @@ classes, counts = np.unique(train_ds, return_counts=True)
 plt.barh( classes_names,counts)
 plt.title('Class distribution in training set')
 
+
+
 import numpy as np
 train_label = np.concatenate([y for x, y in train_ds], axis=0)
 print(train_label.shape)
 
 test_label = np.concatenate([y for x, y in val_ds], axis=0) 
 print(test_label.shape)
+
+
 
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
@@ -142,6 +171,8 @@ print(cm)
 print('Classification Report')
 target_names = ['no','yes']
 print(classification_report(test_label, y_pred, target_names=target_names))
+
+
 
 import seaborn as sns
 #sns.set(font_scale=0.8)
